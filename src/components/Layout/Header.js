20777e054aef9f174b670953720f39474a1a7c44
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-21 18:14:42 +0100

    meal items components implemented

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpeg";
 import HeaderCartButton from "./HeaderCartButton";
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
